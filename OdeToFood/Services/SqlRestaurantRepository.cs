135f960f3b523704240d139ea0f4891633ce1573
shaopard <adi_azrael@yahoo.com>
2019-04-18 14:56:43 +0300

    ASP.NET Core Fundamentals Module 6 - Module complete
    Create OdeToFoodDbContext class to work with Entity Framework.
    Register Entity Framework related services.
    Add data handling service.
    Add Initial Entity Framework migration

---
+++ OdeToFood/Services/SqlRestaurantRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantRepository : IRestaurantRepository
+    {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantRepository(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            _context.Restaurants.Add(newRestaurant);
+            _context.SaveChanges();
+
+            return newRestaurant;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants.OrderBy(restaurant => restaurant.Name);
+        }
+
+        public Restaurant GetRestaurant(int id)
+        {
+            return _context.Restaurants.Find(id);
+        }
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,26 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using OdeToFood.Data;
 using OdeToFood.Services;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+        private readonly string _dbName = "OdeToFood";
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>(); //aici se seteaza implementarea concreta care sa se dea pentru un Igreeter
-            services.AddSingleton<IRestaurantRepository, InMemoryRestaurantData>();
+            services.AddDbContext<OdeToFoodDbContext>(
+                options => options.UseSqlServer(_configuration.GetConnectionString(_dbName)));
+            services.AddScoped<IRestaurantRepository, SqlRestaurantRepository>();
             services.AddMvc();
         }
 
