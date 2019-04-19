6d5e89355c42a5e0dca86e5fd65f25e0513d1270
shaopard <adi_azrael@yahoo.com>
2019-04-19 10:57:43 +0300

    ASP.NET Core Fundamentals Module 9 complete(Course complete)
    Add client side validation using jQuery, jQuery validation and jQuery validation unobtrusive.

--- OdeToFood/ViewComponents/GreeterViewComponent.cs
+++ OdeToFood/ViewComponents/GreeterViewComponent.cs
@@ -1,20 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Services;
+using System;
 
 namespace OdeToFood.ViewComponents
 {
     public class GreeterViewComponent : ViewComponent
     {
         private IGreeter _greeter;
+        private IHostingEnvironment _environment;
 
-        public GreeterViewComponent(IGreeter greeter)
+        public GreeterViewComponent(IGreeter greeter, IHostingEnvironment environment)
         {
             _greeter = greeter;
+            _environment = environment;
         }
 
         public IViewComponentResult Invoke()
         {
-            var model = _greeter.GetMessageOfTheDay();
+            //var model = _greeter.GetMessageOfTheDay();
+
+            var model = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
             return View("Default", model);
         }
     }
